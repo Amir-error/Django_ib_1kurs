a78b27d4312f2c0ddca7d4f5acad8dd6802b215c
Amir-error <76647851+Amir-error@users.noreply.github.com>
2021-05-06 20:56:01 +0300

    Update admin.py

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Articles
+from .models import Articles #импорттируем из нашего репозитория нашу модель
 # Register your models here. Регистрируем нашу модель
-admin.site.register(Articles)
+admin.site.register(Articles) #Регистрируем БДшку
